21a2b8b03bd2b209e2164f3016cfeb609015e1d7
paul-rahul <rahul.paul118@gmail.com>
2018-01-02 07:37:27 +0530

    Routes restructuring

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const express = require('express');
+var express = require('express');
 
-const authRoutes=require('./routes/auth');
-const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup')
 const mongoose = require('mongoose');
 const keys=require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport=require('passport');
 
-const app =express();
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+
+var app =express();
+
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 app.use(cookieSession({
@@ -19,16 +26,40 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 mongoose.connect(keys.mongodb.dbURI,()=>{
     console.log('connected to mongodb');
 });
 
+// Static Folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.authdata = fbRef.getAuth();
+  res.locals.page = req.url;
+  next();
+});
+
+// Route files
+const routes = require('./routes/index');
+const authRoutes=require('./routes/auth');
+const profileRoutes = require('./routes/profile-routes');
+
+app.use('/',routes);
 app.use('/auth',authRoutes);
 app.use('/profile',profileRoutes);
-app.get('/',(req,res)=>{
-    res.render('home',{user:req.user});
-})
 
-app.listen(3000,()=>{
-    console.log('running on 3000');
-})
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+// Run Server
+app.listen(app.get('port'), function(){
+  console.log('Server started on port: '+app.get('port'));
+});
